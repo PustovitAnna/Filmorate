bb4d49a1366ddd3e76219ed2142e136776c2f283
AnnaPustowit <pustowitanna@yandex.ru>
2023-03-30 20:58:52 +0300

    feat : Добавить новую функциональность рекомендации

--- src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.filmorate.storage;
 
+import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.User;
 
 import java.util.Collection;
@@ -7,21 +8,23 @@ import java.util.List;
 
 
 public interface UserStorage {
-    public Collection<User> findAll();
+    Collection<User> findAll();
 
-    public User create(User user);
+    User create(User user);
 
-    public User put(User user);
+    User put(User user);
 
-    public User getUserById(int id);
+    User getUserById(int id);
 
-    public void addFriend(int userId, int friendId);
+    void addFriend(int userId, int friendId);
 
-    public void deleteFriend(int userId, int friendId);
+    void deleteFriend(int userId, int friendId);
 
-    public List<User> getFriends(int id);
+    List<User> getFriends(int id);
 
-    public List<User> getListOfMutualFriends(int userId, int otherUserId);
+    List<User> getListOfMutualFriends(int userId, int otherUserId);
 
     void deleteUser(int userId);
+
+    List<Film> getRecommendation(int id);
 }
